cc3ac8664e0411d76d344c7d16a505eb13a5aa2e
LED <fullchaoz32@yahoo.com>
2012-02-02 16:37:17 +0800

    Inizializing Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticketer::Application.config.secret_token = 'd82060af19f65c1eb7e6ecfccc386c4b7659be413d7f72b0547fdda0c3165d9b353c4b9005c6d660117bea784aaad8b379eb58c9871e88ecc025be1d8d2629ab'
