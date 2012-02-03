2ea4f762c36b079cf1d5936356440ee959c66e4a
LED <fullchaoz32@yahoo.com>
2012-02-04 00:19:33 +0800

    Added title functionality for show page

--- features/creating_projects.feature
+++ features/creating_projects.feature
@@ -3,8 +3,9 @@ In order to have projects to assign tickets to
 As a user
 I want to create them easily 
 
-Scenario: Creating a project
+Scenario: Creating a Project
 	Given I am on the homepage
 	When I navigate to the new project creation page
 	And I create a new project
+	And I should be on the project for the new project
 	Then I should be shown the project created verification
--- features/step_definitions/project_steps.rb
+++ features/step_definitions/project_steps.rb
@@ -8,6 +8,12 @@ When /^I create a new project$/ do
   fill_in('Name', :with => 'TextMate 2')
   click_button('Create Project') 
 end
+
+When /^I should be on the project for the new project$/ do
+  current_path.should == project_path(Project.find_by_name!('TextMate 2'))
+  page.should have_content("TextMate 2 - Projects - Ticketer")
+end
+
 Then /^I should be shown the project created verification$/ do
   page.should have_content("Project has been created.")
 end
